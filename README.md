74624ad7e488774bd02af0dfb89990793be537b7
liqiang <codingli1992@gmail.com>
2017-08-30 18:22:59 +0800

    update configuration

---
+++ README.md
@@ -0,0 +1,7 @@
+# 收集的一些用于学习Canvas的DEMO 
+
+* ## 原作者地址
+
+[Canvas-learning](https://github.com/Array-Huang/canvas-learning)
+
+* ## Demo目录
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const gulp = require('gulp');
+const ghpages = require('gulp-gh-pages');
 const imagemin = require('gulp-imagemin');
 
 gulp.task('imgmin', function() {
     return gulp.src('./src/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe(ghpages());
 });
